531d4ffeded82dad39f095071fd8cabe61d61a34
Thomas Bleijendaal <ThomasBleijendaal@users.noreply.github.com>
2022-09-27 22:56:56 +0200

    Use custom required attribute to avoid collisions and aliases (#26)

--- TerraformPluginDotNet.Test/TestResource.cs
+++ TerraformPluginDotNet.Test/TestResource.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using MessagePack;
 using TerraformPluginDotNet.Resources;
 using TerraformPluginDotNet.Serialization;
-using KeyAttribute = MessagePack.KeyAttribute;
 
 namespace TerraformPluginDotNet.Test;
 
